f73053dd1ae8b0f99450c17080aadabcf4f8843b
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-09-27 21:06:10 -0600

    Configuración inicial del proyecto React y Tailwind

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'safeentry',
+        short_name: 'safeentry',
+        description: 'A simple and secure way to manage your entries.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+})
