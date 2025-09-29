c28fceae64c08e4545301b269412144f98082aea
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-09-29 00:30:10 -0600

    Agregando la ruta para login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/public/LoginPage";
+import RoleRedirect from "./pages/public/RoleRedirect";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <p>Hello World! {count}</p>
-  )
+    <Router>
+      <Routes>
+        {/* 🔹 Pagina de Login */}
+        <Route path="/login" element={ <LoginPage /> } />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
