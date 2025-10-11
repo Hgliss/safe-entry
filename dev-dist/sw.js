d02ebdac3ba2251b2ca1da5c0f5932511c765030
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-11 16:16:02 -0600

    paquetes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b6egshk0u1"
+    "revision": "0.23b6elnn4j"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
